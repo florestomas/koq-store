5ae7cde708ca671616cb275dc6b875684dc8d75d
florestomas <floresmancillatomas@gmail.com>
2026-03-30 18:24:34 -0300

    implement auth component with login form (20%)

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,10 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'auth-component',
   imports: [],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuthComponent { }
