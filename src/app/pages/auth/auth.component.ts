adda240f60d124459a46adb1c8b314831a7bf060
florestomas <floresmancillatomas@gmail.com>
2026-04-02 17:35:12 -0300

    added SVG to the login, deleted lucide

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'auth-component',
+
   imports: [],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
 })
-export class AuthComponent { }
+export class AuthComponent {
+
+}
