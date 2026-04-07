566be917b73e37757651b3307edda477d103fd22
florestmas <floresmancillatomas@gmail.com>
2026-04-06 21:43:34 -0300

    feat: implement reactive login form with loading state

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'auth-component',
-
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
 })
 export class AuthComponent {
+  logInForm = new FormGroup({
+    user: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
 
+  public isLoading = signal(false);
+
+  onSubmit() {
+    this.isLoading.set(true);
+    console.log(this.logInForm.value)
+    setTimeout(() => {
+      this.isLoading.set(false);
+      console.log('holaa');
+    }, 2000);
+  }
 }
