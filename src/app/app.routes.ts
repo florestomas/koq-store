794d41762d4f6bafec734099c4b7e58699560a54
florestomas <floresmancillatomas@gmail.com>
2026-04-10 16:19:39 -0300

    errased caralogo.component (too new)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,14 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
-import { CatalogComponent } from './pages/catalog/catalog.component';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'login',
     component: AuthComponent,
   },
-  {
-    path: 'catalogo',
-    component: CatalogComponent,
-  },
+
   {
     path: '**',
-    redirectTo: '',
+    redirectTo: 'login',
   }
 ];
